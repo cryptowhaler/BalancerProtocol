02d497f92ef95a0a56ac0e4a45cc73b24ca0597d
“pretentiouspunjabiguy@gmail.com” <pretentiouspunjabiguy@gmail.com>
2020-08-29 20:10:29 +0530

    Revert "Revert "Revert "c"""
    This reverts commit 677704c9b2176bf89ad07089879f4f10ce50e477.

--- UI-Pool/src/main.ts
+++ UI-Pool/src/main.ts
@@ -13,38 +13,22 @@ import i18n from '@/i18n';
 import '@/lock';
 import '@/style.scss';
 
-// Infinite scroll displays only a limited amount of rows and shows you more once you hit the bottom of you page.
 Vue.use(infiniteScroll);
 
-// you can use require.context to globally register common base components
 const requireComponent = require.context('@/components', true, /[\w-]+\.vue$/);
-console.log(requireComponent);
-console.log('\n \n');
-
 requireComponent.keys().forEach(fileName => {
   const componentConfig = requireComponent(fileName);
-  const componentName = upperFirst( camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))  );
-  console.log(componentConfig);
-  console.log(componentName);
-  console.log('\n \n');
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))
+  );
   Vue.component(componentName, componentConfig.default || componentConfig);
 });
 
-//vue-jazzicon: A dead-simple Jazzicon component for Vue. This component is made to be identical to the visual 
-// identifiers present in the metamask project, in order to create visual continuity between a dApp and the metamask plugin 
-// which may be used to sign ethereum transactions; (creates logo based on address)
 Vue.component('jazzicon', Jazzicon);
-
 Vue.component('VueSwitch', VueSwitch);
-
-//adding the mixin globally defined in ./mixins.ts
 Vue.mixin(mixins);
-
-// When the page loads, the element with the directive gains focus 
 Vue.directive('autofocus', autofocus);
 
-// Hello, I read that I'd need to add the above config option to disable the (annoying)
-// production mode warning ("You are running Vue in development mode.") 
 Vue.config.productionTip = false;
 
 new Vue({
